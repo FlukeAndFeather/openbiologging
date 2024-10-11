fc3a50478b112d330cdfd63b99ad9ffda7a7b0a1
Max Czapanskiy <maxczapanskiy@gmail.com>
2024-10-11 10:26:58 -0700

    october meeting

--- scripts/download_reviews.R
+++ scripts/download_reviews.R
@@ -1,28 +1,41 @@
 library(googlesheets4)
 library(tidyverse)
 
-gs4_auth("mczapans@ucsc.edu")
+gs4_auth("maxczap@ucsb.edu")
 
+reviewers <- c(
+  "tjl",
+  "acn",
+  "ahr",
+  "arp",
+  "cmh",
+  "ecn",
+  "jkb",
+  "mfc",
+  "tac"
+)
 links <- c(
-  "https://docs.google.com/spreadsheets/d/1JfLDnWzCt9W8yKWXQ3VmEDyHUutYmM4cVIb32bkLma0/edit?usp=drive_link",
-  "https://docs.google.com/spreadsheets/d/192zmhkjoRZ71d_2aDu2PappWWTM9f-CNeZ9j2TLIa5g/edit?usp=drive_link",
-  "https://docs.google.com/spreadsheets/d/1n5DuCEUHHWs8CUaaKrXAjBul6mUvol6JW5TF_xzTtHc/edit?usp=drive_link",
-  "https://docs.google.com/spreadsheets/d/1pLbDA5r9bDx2RoGoNg3pKJCKEGaOCFzkCNSDenSOY7g/edit?usp=drive_link",
-  "https://docs.google.com/spreadsheets/d/17TCcBig2jr4IrTAzRiRj6RifetFeLnlqEsDV5CW1Svk/edit?usp=drive_link",
-  "https://docs.google.com/spreadsheets/d/197tmebQiNloqYq2l_IXhs3YTnOsII9HwV6gE3QLRbC0/edit?usp=drive_link",
-  "https://docs.google.com/spreadsheets/d/1TGV72dx4_eGT-vJxEvBWvBElwpzAVdakfxMepDe3Lvs/edit?usp=drive_link",
-  "https://docs.google.com/spreadsheets/d/1PNfLK1fUxx17ufqDPppMQN-3B64NGu7um9zoKE9ny9w/edit?usp=drive_link",
-  "https://docs.google.com/spreadsheets/d/1r6GAoj-qsQvgRE6vQ-PRSvvbY7pUOVgIZtFVVBKG2Vs/edit?usp=drive_link"
+"https://docs.google.com/spreadsheets/d/1r6GAoj-qsQvgRE6vQ-PRSvvbY7pUOVgIZtFVVBKG2Vs/edit?usp=drive_link",
+"https://docs.google.com/spreadsheets/d/1JfLDnWzCt9W8yKWXQ3VmEDyHUutYmM4cVIb32bkLma0/edit?usp=drive_link",
+"https://docs.google.com/spreadsheets/d/192zmhkjoRZ71d_2aDu2PappWWTM9f-CNeZ9j2TLIa5g/edit?usp=drive_link",
+"https://docs.google.com/spreadsheets/d/1n5DuCEUHHWs8CUaaKrXAjBul6mUvol6JW5TF_xzTtHc/edit?usp=drive_link",
+"https://docs.google.com/spreadsheets/d/1pLbDA5r9bDx2RoGoNg3pKJCKEGaOCFzkCNSDenSOY7g/edit?usp=drive_link",
+"https://docs.google.com/spreadsheets/d/17TCcBig2jr4IrTAzRiRj6RifetFeLnlqEsDV5CW1Svk/edit?usp=drive_link",
+"https://docs.google.com/spreadsheets/d/197tmebQiNloqYq2l_IXhs3YTnOsII9HwV6gE3QLRbC0/edit?usp=drive_link",
+"https://docs.google.com/spreadsheets/d/1TGV72dx4_eGT-vJxEvBWvBElwpzAVdakfxMepDe3Lvs/edit?usp=drive_link",
+"https://docs.google.com/spreadsheets/d/1PNfLK1fUxx17ufqDPppMQN-3B64NGu7um9zoKE9ny9w/edit?usp=drive_link"
 )
 
 # Reviews
-reviews <- map(links,
-               \(x) read_sheet(x,
-                               sheet = "papers",
-                               range = "a2:w8710",
-                               col_types = "c")) %>%
+reviews <- map2(links, reviewers,
+                \(l, r) read_sheet(l,
+                                   sheet = "papers",
+                                   range = "a2:w8710",
+                                   col_types = "c") %>%
+                  mutate(source_sheet = r)) %>%
   list_rbind() %>%
-  mutate(across(everything(), \(x) ifelse(x == "NA", NA, x)))
+  mutate(across(everything(), \(x) ifelse(x == "NA", NA, x))) %>%
+  filter(assigned_to == source_sheet)
 reviews$reviewed <- reviews[, 4:14] %>%
   lapply(is.na) %>%
   reduce(`&`) %>%
